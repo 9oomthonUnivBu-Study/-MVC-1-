ebad72c0bd6c587650fed30f1c005eaa06d75311
SeheeKwon <141599911+seh22@users.noreply.github.com>
2025-05-14 19:50:56 +0900

    feat: 섹션 4

---
+++ servlet/src/main/java/hello/servlet/domain/member/Member.java
@@ -0,0 +1,22 @@
+package hello.servlet.domain.member;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter @Setter
+public class Member {
+    private Long id;
+    private String username;
+    private int age;
+
+    public Member() {
+    }
+
+    public Member(String username, int age) {
+        this.username = username;
+        this.age = age;
+    }
+
+    // id 는 Member 를 회원 저장소에 저장하면 회원 저장소가 할당
+
+}
---
+++ servlet/src/main/java/hello/servlet/web/servletmvc/MvcMemberFormServlet.java
@@ -0,0 +1,21 @@
+package hello.servlet.web.servletmvc;
+
+import jakarta.servlet.RequestDispatcher;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+@WebServlet(name = "mvcMemberFormServlet", urlPatterns = "/servlet-mvc/members/new-form")
+public class MvcMemberFormServlet extends HttpServlet {
+
+    @Override
+    protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String viewPath = "/WEB-INF/views/new-form.jsp"; // /WEB_INF 경로안에 JSP가 있으면 외부에서 직접 JSP를 호출x
+        RequestDispatcher dispatcher = request.getRequestDispatcher(viewPath);
+        dispatcher.forward(request, response);
+    }
+}
