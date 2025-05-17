25223dda35b97796ad00750e1dc3bc9d37a4d7ef
Khaneul1 <kosky1219@gmail.com>
2025-05-17 21:34:45 +0900

    viewPath 수정

--- src/main/java/hello/servlet/web/frontcontroller/v2/controller/MemberFormControllerV2.java
+++ src/main/java/hello/servlet/web/frontcontroller/v2/controller/MemberFormControllerV2.java
@@ -12,9 +12,9 @@ public class MemberFormControllerV2 implements ControllerV2 {
     @Override
     public MyView process(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
-        return new MyView("/WEB-INF/views/save-result.jsp"); //인라인 단축키 ctrl + alt + n
+        return new MyView("/WEB-INF/views/new-form.jsp"); //인라인 단축키 ctrl + alt + n
         //인라인 전 코드
-        //MyView myView = new MyView("/WEB-INF/views/save-result.jsp");
+        //MyView myView = new MyView("/WEB-INF/views/new-form.jsp");
         //return myView;
 
         //지저분한 로직 없이 경로만 넣으면 되니까 깔끔해짐!!
